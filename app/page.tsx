c47ac31ee76a3001bb0ef1f823c64536d95fe767
AnuragBalhra <anurag.1@iitj.ac.in>
2025-02-23 10:43:32 +0000

    minor ui update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Stats from '@/app/components/home/Stats';
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="flex flex-col min-h-screen bg-white justify-center w-full">
       <Hero />
       <Features />
       <Stats />
